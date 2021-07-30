e3159f27c1da2c90c6ce838fcaa5e7a12b071b30
Supravat <53163178+supravat1@users.noreply.github.com>
2021-07-30 15:38:52 +0530

    Update ReadExcelFileUsingPOI_API.java

--- Selenium_Webdriver/src/test/java/com/excel/ReadExcelFileUsingPOI_API.java
+++ Selenium_Webdriver/src/test/java/com/excel/ReadExcelFileUsingPOI_API.java
@@ -1,72 +1,72 @@
-package com.excel;
+// package com.excel;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.concurrent.TimeUnit;
+// import java.io.File;
+// import java.io.FileInputStream;
+// import java.io.FileNotFoundException;
+// import java.util.concurrent.TimeUnit;
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+// import org.apache.poi.ss.usermodel.Row;
+// import org.apache.poi.xssf.usermodel.XSSFSheet;
+// import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.testng.annotations.Test;
 
-public class ReadExcelFileUsingPOI_API {
+// public class ReadExcelFileUsingPOI_API {
 
-@Test
+// @Test
 	
-	public void script1() throws Exception
-	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\supra\\Downloads\\ChromeDriver_selenium\\chromedriver.exe");
+// 	public void script1() throws Exception
+// 	{
+// 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\supra\\Downloads\\ChromeDriver_selenium\\chromedriver.exe");
 		
-		WebDriver driver = new ChromeDriver();
+// 		WebDriver driver = new ChromeDriver();
 
 		
-		String file1 = ("C:\\Users\\supra\\Desktop\\HCL_Selenium_Training_Assignments\\Excel_POI.xlsx");
+// 		String file1 = ("C:\\Users\\supra\\Desktop\\HCL_Selenium_Training_Assignments\\Excel_POI.xlsx");
 		
-		File f1 = new File(file1);
+// 		File f1 = new File(file1);
 		
-		FileInputStream fis = new FileInputStream(f1);
+// 		FileInputStream fis = new FileInputStream(f1);
 		
-		XSSFWorkbook workbook1 = new XSSFWorkbook(fis);
+// 		XSSFWorkbook workbook1 = new XSSFWorkbook(fis);
 		
-	 	XSSFSheet sheet1 = workbook1.getSheetAt(0);
+// 	 	XSSFSheet sheet1 = workbook1.getSheetAt(0);
 	 	
-	 	int rowcount = sheet1.getLastRowNum();
-	 	System.out.println(rowcount);
-//	 	String s1 = sheet1.getRow(1).getCell(0).getStringCellValue();
-//	 	System.out.println(s1);
+// 	 	int rowcount = sheet1.getLastRowNum();
+// 	 	System.out.println(rowcount);
+// //	 	String s1 = sheet1.getRow(1).getCell(0).getStringCellValue();
+// //	 	System.out.println(s1);
 
-	 	for(int i =1; i< rowcount+1; i++)
-	 	{
-	 		String username = sheet1.getRow(i).getCell(0).getStringCellValue();
-	 		String password = sheet1.getRow(i).getCell(1).getStringCellValue();
+// 	 	for(int i =1; i< rowcount+1; i++)
+// 	 	{
+// 	 		String username = sheet1.getRow(i).getCell(0).getStringCellValue();
+// 	 		String password = sheet1.getRow(i).getCell(1).getStringCellValue();
 	 		
-//	 		System.out.println(username);
-//	 		System.out.println(password);
+// //	 		System.out.println(username);
+// //	 		System.out.println(password);
 	 		
-	 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+// 	 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 	 		
-	 		driver.get("https://www.netflix.com/in/login");
+// 	 		driver.get("https://www.netflix.com/in/login");
 	 		
-	 		driver.manage().window().maximize();
+// 	 		driver.manage().window().maximize();
 	 		
-	 		driver.findElement(By.id("id_userLoginId")).clear();
+// 	 		driver.findElement(By.id("id_userLoginId")).clear();
 	 		
-	 		driver.findElement(By.id("id_userLoginId")).sendKeys(username);
+// 	 		driver.findElement(By.id("id_userLoginId")).sendKeys(username);
 	 		
-	 		driver.findElement(By.id("id_password")).sendKeys(password);
+// 	 		driver.findElement(By.id("id_password")).sendKeys(password);
 	 		
-			driver.findElement(By.xpath("//button[@class=\"btn login-button btn-submit btn-small\"]")).click();
+// 			driver.findElement(By.xpath("//button[@class=\"btn login-button btn-submit btn-small\"]")).click();
 
 			
-	 	}
+// 	 	}
 	 	
-	 	driver.close();
-		workbook1.close();
+// 	 	driver.close();
+// 		workbook1.close();
 		
 		
-	}
-}
+// 	}
+// }
